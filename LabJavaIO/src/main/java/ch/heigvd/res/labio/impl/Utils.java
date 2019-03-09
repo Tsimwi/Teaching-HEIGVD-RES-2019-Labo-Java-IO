0a3293f1b720d687aaba8ba57d4982470d0a10cb
Tsimwi <caroline.monthoux@heig-vd.ch>
2019-03-09 14:50:15 +0100

    Add getNextLine method, UtilsTest OK

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -3,24 +3,52 @@ package ch.heigvd.res.labio.impl;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class Utils {
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to extract
+     * the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the argument does not
+     * contain any line separator, then the first element is an empty string.
+     */
+    public static String[] getNextLine(String lines) {
+        String[] array = new String[2];
+        int separatorPosition = -1;
+
+        for (int i = 0; i < lines.length(); ++i) {
+
+            if (lines.charAt(i) == '\n') {
+                separatorPosition = i;
+                break;
+            }
+
+            if (lines.charAt(i) == '\r') {
+                if ((i + 1) < lines.length() && (lines.charAt(i + 1) == '\n')) {
+                    separatorPosition = i + 1;
+                } else {
+                    separatorPosition = i;
+                }
+
+                break;
+            }
+        }
+
+        if (separatorPosition == -1) {
+            array[0] = "";
+            array[1] = lines;
+        } else {
+            array[0] = lines.substring(0, separatorPosition + 1);
+            array[1] = lines.substring(separatorPosition + 1);
+        }
+
+        return array;
+    }
 
 }
